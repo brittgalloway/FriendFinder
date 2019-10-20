a74719a49db21bc2b6e6acd5b3c99e7c9f996fe2
BrittGall <crlnfllr@gmail.com>
2019-10-20 17:50:31 -0500

    corrected links

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,6 @@
 const friendData = require("../data/friends.js");
 module.exports = function(app) {
-  app.get("/survey", function(req, res) {
+  app.get("/api/survey", function(req, res) {
     res.json(friendData);
-  });
-  app.post("api/clear", function(request, response) {
-    friendData.push(req.body);
   });
 };
