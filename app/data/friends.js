98b2573607f62ec7d2118d3f7fbe01f7abb5aec0
BrittGall <crlnfllr@gmail.com>
2019-10-20 14:53:31 -0500

    added friends array

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+const friends = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Jason",
+    "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 1, 2, 4, 5, 3, 2, 1, 2, 3]
+  },
+  {
+    name: "Cati",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 2, 5, 5, 4, 2, 1, 4, 5, 2]
+  },
+  {
+    name: "Jazzy",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 5, 2, 2, 1, 5, 4, 1, 2, 5]
+  },
+];
