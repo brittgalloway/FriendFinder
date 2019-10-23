61973a9d5773533747d282a9249a3088ac86657c
BrittGall <crlnfllr@gmail.com>
2019-10-22 22:13:36 -0500

    added post route and required path

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,10 @@
 const friendData = require("../data/friends.js");
+const path = require("path");
 module.exports = function(app) {
   app.get("/api/survey", function(req, res) {
     res.json(friendData);
+  });
+  app.post("/api/survey", function(req, res) {
+    friendData.push(req.body);
   });
 };
